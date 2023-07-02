3f1db6e175284121e94df443be3c7567ad59f3e0
Fran <20220326@ricaldone.edu.sv>
2023-07-02 06:42:14 -0600

    Avance en la vista del login, modelo del login y la conexion hecha con la base de datos-

---
+++ src/Modelo/mLogin.java
@@ -0,0 +1,22 @@
+
+package Modelo;
+
+import java.sql.*;
+
+public class mLogin {
+    private int idUsuario;
+    private int idTipo;
+    private String usuario;
+    private String contra;
+    private String correo;
+    
+    public void VerifTipo(){
+        try{
+            //PreparedStatement addReserva = conx.getConexion().prepareStatement();
+            
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    
+}
